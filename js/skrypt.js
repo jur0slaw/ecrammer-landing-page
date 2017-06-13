25bc1657c32732f3a52c663d464a40c668b5d6b3
jur0slaw <jerjurko@gmail.com>
2017-06-13 16:01:10 +0200

    Added scrolling

--- js/skrypt.js
+++ js/skrypt.js
@@ -3,10 +3,10 @@ $( document ).ready(function() {
     document.onresize = function() {myFunction()};
 
     function myFunction() {
-    $("intro-header").height(window.height()-$("navbar").height);
-    $("intro-header").width($("document").width());
-}
-    var last_known_scroll_position = 0;
+        $("intro-header").height(window.height()-$("navbar").height);
+        $("intro-header").width($("document").width());
+    }
+        var last_known_scroll_position = 0;
         var ticking = false;
 
         function doSomething(scroll_pos) {
@@ -17,6 +17,7 @@ $( document ).ready(function() {
             $('html, body').animate({
             scrollTop: $("#info").offset().top
             }, 2000);
+            
         });
 
         window.addEventListener('scroll', function(e) {
@@ -29,6 +30,25 @@ $( document ).ready(function() {
           }
           ticking = true;
         });
+        
+     $(window).scroll( function(){
     
+        /* Check the location of each desired element */
+        $('.content-section-a').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+      
     
 });
